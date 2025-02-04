2e6bdb19223cb252b1e2e3fcf528d080911cd877
StepanBaraban <stepanbaraban2015@gmail.com>
2025-02-04 20:06:44 +0300

    Update test..py
    Добавил функции из файла timerbot.py

--- test..py
+++ test..py
@@ -4,6 +4,7 @@
 # It echoes any incoming text messages.
 
 import telebot
+import time, threading, schedule
 
 API_TOKEN = 'token'
 
@@ -23,6 +24,40 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     bot.reply_to(message, message.text)
+
+
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hi! Use /set <seconds> to set a timer")
+
+
+def beep(chat_id) -> None:
+    """Send the beep message."""
+    bot.send_message(chat_id, text='Beep!')
+
+
+@bot.message_handler(commands=['set'])
+def set_timer(message):
+    args = message.text.split()
+    if len(args) > 1 and args[1].isdigit():
+        sec = int(args[1])
+        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
+    else:
+        bot.reply_to(message, 'Usage: /set <seconds>')
+
+
+@bot.message_handler(commands=['unset'])
+def unset_timer(message):
+    schedule.clear(message.chat.id)
+
+
+if __name__ == '__main__':
+    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 bot.infinity_polling()
